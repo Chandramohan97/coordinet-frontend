1245b58cf6fb138c2d512e3f4015e5086f276f9f
Chandramohan97 <chandramsriv@gmail.com>
2023-02-19 12:27:43 +0530

    style : customising login page.

---
+++ src/pages/LoginPage/LoginPageStyles.js
@@ -0,0 +1,9 @@
+// Creating custom button for LoginPage
+
+import styled from "styled-components";
+import { Button } from "../../Components/button/ButtonStyles";
+
+export const Login = styled(Button)`
+  width: 20vw;
+  height: 5vh;
+`;
