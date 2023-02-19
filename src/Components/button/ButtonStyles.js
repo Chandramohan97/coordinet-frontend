1d5438fa493c7a6e592db0df7428a650fba80070
Chandramohan97 <chandramsriv@gmail.com>
2023-02-19 12:29:11 +0530

    feat : Creating button components.

---
+++ src/Components/button/ButtonStyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  color: white;
+  height: 40px;
+  width: 100px;
+  background-color: #31bfc8;
+  text-align: center;
+  font-size: 12px;
+  margin: 30px 10px;
+  border-radius: 5px;
+`;
