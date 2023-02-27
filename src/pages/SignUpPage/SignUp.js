a0f1a2c1a1092fd0531717df06edc969e6ab44ec
Chandramohan97 <chandramsriv@gmail.com>
2023-02-27 11:07:32 +0530

    feat : SignUp Page.

--- src/pages/SignUpPage/SignUp.js
+++ src/pages/SignUpPage/SignUp.js
@@ -1,10 +1,36 @@
-// import React from "react";
-import SignUpForm from "../../Components/SignUpForm/SignUpForm";
-
 import React from "react";
+import SignUpForm from "../../Components/SignUpForm/SignUpForm";
+import Navbar from "../../Components/Navbar/Navbar";
+import CustomButton from "../../Components/button/CustomButton";
+import { Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return <SignUpForm />;
+  return( 
+    <div className="SignUp">
+    <Flex my={"30px"} mx="30px" flexDirection={"row"} justifyContent="space-between"  background="#F7F8FC">
+      <Navbar/>
+      <Link to="/login">
+            <CustomButton
+                color="white"
+                height="5vh"
+                width="7vw"
+                bg="#31bfc8"
+                _hover={{ bg: "#2a9ca1" }} //blue-green
+                _active={{ bg: "#555555" }}
+                textAlign="center"
+                fontSize="15px"
+                margin="15px 10px"
+                borderRadius="5px"
+                value="Login"
+            />
+        </Link>
+      </Flex>
+      <SignUpForm />
+    
+    </div>
+  
+  );
 };
 
 export default SignUp;
