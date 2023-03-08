b0d59c940122d3df9655a6301f516d3ec852020c
Chandramohan97 <chandramsriv@gmail.com>
2023-03-08 12:56:54 +0530

    feat : adding Column and cards(Task.js)

--- src/Components/KanbanTest/Task/Task.js
+++ src/Components/KanbanTest/Task/Task.js
@@ -8,8 +8,9 @@ import './Task.css'
 import JoditEditor from 'jodit-react';
 import Jodit from './Jodit';
 import Comments from '../../Comments/Comments';
+import { Draggable } from 'react-beautiful-dnd';
 
-function Task({ task }) {
+function Task({ cards,index }) {
   const [open,setOpen] = React.useState(false);
   const [visibility,setVisibility]= React.useState(false)
   const [description,setDescrition]= React.useState("");
@@ -25,64 +26,84 @@ function handleClose() {
     setOpen(false);
   }
   return (
-  // <Draggable>
-    <Box 
-      bg="white"  
-      mb="2" 
-      rounded="md"
-      _hover={{backgroundColor:"lightgrey"
-      }}
-      >
-      <Textarea
-       focusBorderColor="transparent"
-       resize="none" 
-       _hover={{cursor:"pointer"}}
-       onClick={()=>handleOpen()}
-       defaultValue={task.content}
-       //removes the 3 slanted lines from TextArea       
-      />
-      <Modal isOpen={open} onClose={handleClose} size="custom" >
-        <ModalOverlay />
-        <ModalContent maxW="75vw" h="auto" maxH="none">
-            <Input
-             width={"40vw"} height={"6vh"} fontSize={"20px"} defaultValue={task.content} my="2vh" ml={"2vw"} border="0.1px solid"     placeholder='Short Description' isRequired _hover={{backgroundColor:"lightgrey"}} _active={{backgroundColor:"initial"}}
-             borderColor="transparent"
-            />
-            <ModalBody ml="0.5vw">
-              <HStack>
-                <Flex direction={"row"} >
-                  <Flex flexDirection={"column"} alignItems="center" justifyContent={"space-evenly"} gap="10px">
-                    <Text fontSize={"m"} textAlign={"left"} >Description</Text>
-                     { visibility ? 
-                     <>
-                     <Jodit />
-                     <HStack>
-                      <Button backgroundColor={"blue.100"} onClick={()=>{setVisibility(false)}}>Save</Button>
-                      <Button onClick={()=> {setVisibility(false)}}>Cancel</Button>
-                      </HStack>
-                     </>
-                      :
-                     <Input 
-                     width={"40vw"}
-                     _hover={{backgroundColor:"lightgrey"}}
-                     placeholder='Enter a title here' 
-                     onClick={()=> {setVisibility(true)}} 
-                     borderColor="transparent"
-                     /> }
-                    <Text as="p" textAlign={"left"}>Comments</Text>
-                    <Comments/>
-                    
-                  </Flex>
+    <>
+     <Draggable draggableId={cards.id} key={cards.id} index={index} >
+       {(provided)=>(
+          // <Box className='cards' //   bg="white"     mb="2"      rounded="md"        _hover={{backgroundColor:"lightgrey"    //   }}
+          //   // key={task.id}
+          //   >
+            <div className='cards'>
+            <Textarea
+              bg="white"
+              rounded={"md"}
+              margin={"5px 0px"}
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              //  _hover={{backgroundColor:"lightgrey"}}
+              focusBorderColor="transparent"
+              resize="none" 
+              _hover={{cursor:"pointer" , bg  :"lightblue"}}
+              onClick={()=>handleOpen()}
+              defaultValue={cards.label}
+              //removes the 3 slanted lines from TextArea       
+              />
+              </div>
+       )}
+        </Draggable>
+              <Modal isOpen={open} onClose={handleClose} size="custom" >
+                <ModalOverlay />
+                <ModalContent maxW="75vw" h="auto" maxH="none">
+                    <Input
+                    width={"40vw"} height={"6vh"} fontSize={"20px"} defaultValue={cards.label} my="2vh" ml={"2vw"} border="0.1px solid"     placeholder='Short Description' isRequired _hover={{backgroundColor:"lightgrey"}} _active={{backgroundColor:"initial"}}
+                    borderColor="transparent"
+                    />
+                    <ModalBody ml="0.5vw">
+                      <HStack>
+                        <Flex direction={"row"} >
+                          <Flex flexDirection={"column"} alignItems="center" justifyContent={"space-evenly"} gap="10px">
+                            <Text position={"relative"} left="-17vw" fontSize={"m"} textAlign={"left"} fontWeight="bold" >Description</Text>
+                            { visibility ? 
+                            <>
+                            <Jodit />
+                            <HStack position={"relative"} left="-15vw">
+                              <Button backgroundColor={"blue"} color="white" onClick={()=>{setVisibility(false)}}
+                              height="30px" borderRadius={"4px"}
+                              >Save</Button>
+                              <Button height="30px" borderRadius={"4px"} onClick={()=> {setVisibility(false)}}>Cancel</Button>
+                              </HStack>
+                            </>
+                              :
+                            <Input 
+                            width={"40vw"}
+                            _hover={{backgroundColor:"lightgrey"}}
+                            placeholder='Enter a title here' 
+                            onClick={()=> {setVisibility(true)}} 
+                            borderColor="transparent"
+                            /> }
+                            <Text as="p" position={"relative"} left="-17.3vw" fontWeight={"bold"}>Comments</Text>
+                            <Comments/>
+                            
+                          </Flex>
 
-                </Flex>
-              </HStack>
-            </ModalBody>
-        <ModalCloseButton />
-        </ModalContent>
-         
-      </Modal>      
-    </Box>
-  // </Draggable>
+                          <Flex direction={"column"}>
+                            <div className='status'>
+                              <Text>Status</Text>
+                              <Button></Button>
+                            </div>
+
+                          </Flex>
+                        </Flex>
+                      </HStack>
+                    </ModalBody>
+                <ModalCloseButton />
+                </ModalContent>   
+              </Modal>
+                 
+          {/* // </Box> */}
+          </>
+       
+   
   );
 }
 
