ff5ebb6f0708e82c065f132751b1d2a65927e4f8
Chandramohan97 <chandramsriv@gmail.com>
2023-03-08 12:56:22 +0530

    feat : Adding drag and drop functionality to drag and drop elements.

--- src/Components/KanbanTest/App1.js
+++ src/Components/KanbanTest/App1.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box, Heading, Textarea } from '@chakra-ui/react'
-import { data } from './data'
+import { Board } from './data'
 import Column from './Column'
 import { Flex } from '@chakra-ui/react'
 import { DragDropContext,Droppable,Draggable } from 'react-beautiful-dnd'
@@ -8,51 +8,33 @@ import {render,screen} from '@testing-library/react'
 import { Editor } from 'react-draft-wysiwyg'
 import { EditorState } from 'react-draft-wysiwyg'
 
+
 const App1 = () => {
+
+  const [boards,SetBoards] = React.useState(Board);
+
+  // const addCards = (title)
+  const onEnd = (result) =>{
+    console.log(result);
+  }
   return (
     <Box p ="4">
         <Heading as="h1" mb="4">
             Kanban Board
         </Heading>
-        <Flex>
-        {data.map((column) =>(
-                <Column key={column.id} column={column}/>    
-            ))}
-        </Flex>
+        <DragDropContext onDragEnd={onEnd}>
+            <Flex className='col'>
+                {boards.map((column,index) =>(
+                        <Column key={column.id}  column={column} index={index}/>    
+                    ))}
+              </Flex>
+        </DragDropContext>
     </Box>
 
   )
 }
 
 
-// const App1=()=>{
-//   const [boardData,setData] = React.useState(data);
 
-//   return(
-//       <div className='app1'>
-//         <DragDropContext>
-//           <Droppable droppableId="board">
-//             {(provided) => (
-//               <Flex flexDirection={"column"} gap="20px" {...provided.droppableProps} ref={provided.innerRef}>  
-//                 {boardData.map((column,index) => (
-//                   <Draggable key={column.id} draggableId={`Column-${column.id}`} index={index}>
-//                     {(provided) => (
-//                       <Textarea 
-//                         {...provided.dragHandleProps}
-//                         defaultValue={column.title}
-//                         {...provided.draggableProps}
-//                         ref={provided.innerRef}
-//                     ></Textarea>          
-//                   )}
-//                   </Draggable>
-//                 ))}
-//                 {provided.placeholder} 
-//               </Flex>
-//               )}
-//           </Droppable>
-//           </DragDropContext>
-//         </div>
-      
-//   )
-// }
+
 export default App1;
