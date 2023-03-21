fe1c8650da9286bfb285642f5ae9b08689382782
Chandramohan97 <chandramsriv@gmail.com>
2023-03-21 16:57:10 +0530

    checking changes in Hero.js

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,15 +1,15 @@
-import { Heading,Image ,Box,Text} from '@chakra-ui/react';
+import { Heading,Image ,Box,Text, Grid} from '@chakra-ui/react';
 import React from 'react';
 import img from '../../Asssets/landingPage(1).png'
 const Hero = () =>{
   return(
-    <Box className='hero' display={"flex"} flexDirection="row" justifyContent={"space-evenly"} mt="10vh">
-      <Box position={"relative"} left="-10vw" rowGap={"70px"}>
+    <Grid className='hero' display={"flex"} flexDirection="row" justifyContent={"space-evenly"} mt="10vh">
+      <Grid position={"relative"} left="-10vw" rowGap={"70px"}>
         <Heading>Coordinet</Heading>
         <Text>Cordinate your tasks better</Text>
       </Box>
       <Image src={img} width="fit-content" />
-    </Box>
+    </Grid>
   )
 }
 
