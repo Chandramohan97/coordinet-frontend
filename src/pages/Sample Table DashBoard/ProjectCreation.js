57e8342189ac6e8710dd9a35304c32db477dfc9d
Chandramohan97 <chandramsriv@gmail.com>
2023-03-26 04:36:50 +0530

    name change of SampleDashboard to ProjectCreation

--- src/pages/Sample Table DashBoard/ProjectCreation.js
+++ src/pages/Sample Table DashBoard/ProjectCreation.js
@@ -3,14 +3,18 @@ import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import StaticTable from '../../Components/Table/StaticTable'
+import { Link } from "react-router-dom";
 
-const SampleTableDashboard = () => {
+const ProjectCreation = () => {
+
+  const [project,setProject] = React.useState("");
+
   return (
     <div className='sampleTable'>
         <Heading textAlign={"left"} fontFamily={"Inder"} fontSize="40px" ml="2vw" mt={"5vh"}>COORDINET</Heading>
         <HStack gap="5vw">
             <VStack pl="3vw">
-                <Text fontSize={"35px"} textAlign="left"     width="21vw">What's your team working on ?</Text>
+                <Text fontSize={"35px"} textAlign="left" width="21vw">What's your team working on?</Text>
                 <Input 
                 placeholder='Name of the Project, eg. First Project'
                 focusBorderColor='transparent'
@@ -19,25 +23,35 @@ const SampleTableDashboard = () => {
                 width={"20vw"} 
                 rounded="10px" 
                 height={"40px"} 
-                backgroundColor="white"/>   
+                backgroundColor="white"
+                onChange={(e) => setProject(e.target.value)}
+                />   
             </VStack>
             <Flex gap="0px" flexDirection={"row"} ml="20vw">
-                <Sidebar/>
-                <StaticTable/>
+                <Sidebar project={project} />
+                <StaticTable taskList={""}/>
             </Flex>
         </HStack>
-        <Button 
-        color="white"
-        bg="#31bfc8"
-        mt="15px" 
-        _hover={{ bg: "#2a9ca1" }} //blue-green
-        _active={{ bg: "#555555" }}
-        height={"38px"} 
-        width="100px" 
-        ml="81.5vw" >Next</Button>
+
+        <Link to={{
+          pathname: "/taskCreation",
+          state: {project}
+        }}>
+          <Button 
+          color="white"
+          bg="#31bfc8"
+          mt="15px" 
+          _hover={{ bg: "#2a9ca1" }} //blue-green
+          _active={{ bg: "#555555" }}
+          height={"38px"} 
+          width="100px" 
+          ml="81.5vw">
+            Next
+          </Button>
+        </Link>
     </div>
 
   )
 }
 
-export default SampleTableDashboard
+export default ProjectCreation;
