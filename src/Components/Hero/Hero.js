ccafc4a39fccbec65b79ea87bf7af0d7179e8eb8
Chandramohan97 <chandramsriv@gmail.com>
2023-03-31 12:17:33 +0530

    style : Media Query for Hero Image Section.

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -4,18 +4,20 @@ import img from '../../Asssets/landingPage(1).png'
 
 const Hero = () =>{
   return(
-    <Box className='hero' display={"flex"} flexDirection="row" justifyContent={"space-evenly"} mt="8vh">
-      <Box position={"relative"} left="-5vw" rowGap={"70px"} >
+    <Box className='hero' display={"flex"} flexDirection={{base:"column",md:"row"}} justifyContent={"space-between"} mt="8vh"   >
+      <Box position={"relative"}  ml="8vw" textAlign={{base: "center", md: "left"}} >
         {/* <Heading>COORDINATE</Heading> */}
-        <Text fontSize={"6xl"} color={"blue.600"} fontWeight="semibold" ml="-1vw">Coordinate</Text>
-        <Text fontSize={"6xl"} color={"blue.600"} fontWeight="semibold">Tasks Better</Text>
-        <Text fontSize={"3xl"} color={"blue.600"} fontWeight="semibold" ml="-1vw">Empowering Teams <br></br> to achieve success, <br></br>
+        <Text display={"block"} 
+        fontFamily="Inder sans-serif" fontSize={{base:"small",md:"4xl"}} color={"black.600"} fontWeight="semibold">COORDINATE</Text>
+        <Text display={"block"} fontFamily="Inder sans-serif" fontSize={{base:"small  ",md:"4xl"}} color={"black.600"} fontWeight="semibold">TASKS BETTER</Text>
+        <Text fontSize={{base:"small",md:"2xl"}} fontFamily="Inder sans-serif" color={"black.600"} fontWeight="semibold">Empowering Teams <br></br> to achieve success, <br></br>
         one project at a time
         </Text>
         {/* <Text fontSize={"5xl"}>TASKS</Text> */}
         {/* <Text fontSize={"5xl"}>Better</Text> */}
       </Box>
-      <Image src={img} width="fit-content" height={"60vh"}/>
+      <Image src={img} alt="Project Management" marginLeft={{base:"10vw",md:"none"}}
+       mr="12vw" width={{base: "auto", md: "fit-content"}} height={{base: "auto", md: "60vh"}} />
     </Box>
   )
 }
