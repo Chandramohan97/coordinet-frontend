1c3876b02d8b49968df7d2c592d79876905b3432
Chandramohan97 <chandramsriv@gmail.com>
2023-04-04 04:02:49 +0530

    media query : applying media query to SideBar.js

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -10,22 +10,25 @@ function Sidebar(props) {
 
   return (
   <>
-    <Box height="70vh" width={"12vw"} backgroundColor="#191970"  >
+    <Box height={{ base: "50vh", md: "70vh" }} width={{ base: "26vw", md: "8vw" }} backgroundColor="#191970"  >
       <Flex flexDirection="row" mt="10vh" ml="5px">
-        <Icon as={FaHome} fill="white" display={"inline"}/>
-        <Text color={"white"} position={"relative"} top="-5px" left="5px">Home</Text>    {/* To align icon and Text together */}
+        <Icon as={FaHome} fill="white"  display={"inline"}/>
+         {/* To align icon and Text together */}
+         <Text color={"white"} fontFamily={"Inder"} fontSize={{base:"14px",md:"14px"}} position={"relative"} top={{ base: "-2.5px", md: "-2.5px" }} 
+         left={{ base: "4px", md: "4px" }}>Home</Text>   
       </Flex>
-      <Divider></Divider>
+      {/* <Divider></Divider> */}
       {
         propsLength === 1 ? 
         <Box display={"flex"} flexDirection="row">
           {/* <Image src={task} alt="task" fill={"white"} mt="1vh"/> */}
-          <Text mt="1vh" color="white" fontSize={"16px"} fontFamily={"Inder"} textAlign="left" ml="10px" marginTop="12px">
+          <Text mt="1vh" color="white" fontSize={{base:"10px",md:"13px"}} fontFamily={"Inder"} textAlign="left" ml="10px" marginTop="12px">
             {props.project}
           </Text>
         </Box> : <p>{""}</p>
       }
     </Box>
+    <Divider borderColor="grey" mt="12.9vh" position={"absolute"} width={{base:"104%",md:"48%"}}></Divider>
     
     </>
   )
