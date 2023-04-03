a6b953b5dc7dd6a9471b64bda0fbbcd142666732
Chandramohan97 <chandramsriv@gmail.com>
2023-04-04 04:02:22 +0530

    media query : Applying media query to TaskCreation

--- src/pages/TaskCreation/TaskCreation.js
+++ src/pages/TaskCreation/TaskCreation.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import StaticTable from '../../Components/Table/StaticTable'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-import { Flex,Heading, HStack,Input,Text, VStack,Button } from '@chakra-ui/react'
+import { Flex,Heading, HStack,Input,Text, VStack,Button,Box } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
 
 const TaskCreation = () => {
@@ -13,50 +14,62 @@ const TaskCreation = () => {
   const  location  = useLocation();
   const  {project} = location.state;
   // console.log(project);
-  // const { state } = location || {};
-  // const myStateVariable = state ? state.myStateVariable : null;
-  // console.log(myStateVariable);
-  // const location = useLocation();
-  // const [project, setProject] = React.useState(location.state.project || "")
-
+  
   return (
-    <div className='taskcreation'>
-        <Heading textAlign={"left"} fontFamily={"Inder"} fontSize="40px" ml="2vw" mt={"5vh"}>COORDINET</Heading>
+    <Box className='taskCreation' px={{base: "2", md: "8"}}>
+        <Heading textAlign={"left"} fontFamily={"Inder"} 
+        fontSize={{base:"16px",md:"40px"}} width={"fit-content"} ml="2vw" mt={"5vh"}>COORDINET</Heading>
         <HStack gap="5vw">
             <VStack pl="3vw">
-                <Text fontSize={"35px"} textAlign="left" fontFamily={"Inder"} width="21vw">Creating Tasks</Text>
+                <Text width={{base:"100px",md:"21vw"}}
+                 fontSize={{base:"12px", md:"35px"}} textAlign="left" fontFamily={"Inder"} >Creating Tasks</Text>
                 <Input 
-                placeholder='First Task'
-                focusBorderColor='transparent'
-                marginRight={"20px !important"} 
-                width={"20vw"} 
-                rounded="10px" 
-                fontFamily={"Inder"}
-                height={"40px"} 
-                backgroundColor="white"
-                onChange={(e) => setTask1(e.target.value)}
+                    fontSize={{base:"8px",md:"15px"}}
+                    placeholder='First Task'
+                    borderColor={"lightgrey"}
+                    focusBorderColor='lightgrey'
+                    marginRight={"20px !important"} 
+                    width={{base:"110px",md:"20vw"}}
+                    textAlign={"left"}
+                    rounded={{base:"7px",md:"10px"}} 
+                    fontFamily={"Inder"}
+                    height={{base:"22px",md:"40px"}} 
+                    backgroundColor="white"
+                    position={"relative"}
+                    left={{base:"15px",md:"-1px"}}
+                    onChange={(e) => setTask1(e.target.value)}
                 />
                 <Input 
-                placeholder='Second Task'
-                focusBorderColor='transparent'
-                marginRight={"20px !important"} 
-                width={"20vw"} 
-                fontFamily={"Inder"}
-                rounded="10px" 
-                height={"40px"} 
-                backgroundColor="white"
-                onChange={(e) => setTask2(e.target.value)}
+                    fontSize={{base:"8px",md:"15px"}}
+                    placeholder='Second Task'
+                    borderColor={"lightgrey"}
+                    focusBorderColor='lightgrey'
+                    marginRight={"20px !important"} 
+                    width={{base:"110px",md:"20vw"}}
+                    textAlign={"left"}
+                    rounded={{base:"7px",md:"10px"}} 
+                    fontFamily={"Inder"}
+                    height={{base:"22px",md:"40px"}} 
+                    backgroundColor="white"
+                    position={"relative"}
+                    left={{base:"15px",md:"-1px"}}
+                    onChange={(e) => setTask2(e.target.value)}
                 /> 
                 <Input 
-                placeholder='Third Task'
-                focusBorderColor='transparent'
-                fontFamily={"Inder"}
-                marginRight={"20px !important"} 
-                width={"20vw"} 
-                rounded="10px" 
-                height={"40px"}
-                backgroundColor="white"
-                onChange={(e) => setTask3(e.target.value)}
+                   fontSize={{base:"8px",md:"15px"}}
+                   placeholder='Third Task'
+                   borderColor={"lightgrey"}
+                   focusBorderColor='lightgrey'
+                   marginRight={"20px !important"} 
+                   width={{base:"110px",md:"20vw"}}
+                   textAlign={"left"}
+                   rounded={{base:"7px",md:"10px"}} 
+                   fontFamily={"Inder"}
+                   height={{base:"22px",md:"40px"}} 
+                   backgroundColor="white"
+                   position={"relative"}
+                   left={{base:"15px",md:"-1px"}}
+                  onChange={(e) => setTask3(e.target.value)}
                 />  
             </VStack>
             <Flex gap="0px" flexDirection={"row"} ml="20vw">
@@ -64,17 +77,26 @@ const TaskCreation = () => {
                 <StaticTable taskList={[task1,task2,task3]}/>
             </Flex>
         </HStack>
-        <Button 
-        color="white"
-        bg="#31bfc8"
-        _hover={{ bg: "#2a9ca1" }} //blue-green
-        _active={{ bg: "#555555" }}
-        mt="15px" 
-        height={"38px"} 
-        width="100px" 
-        ml="81.5vw" >Next</Button>
+
+        <Link to="/viewingOptions" state={{project : project , tasks:[task1,task2,task3]}}   >
+          <Button 
+             color="white"
+             bg="#31bfc8"
+             position={{base:"fixed",md:"relative"}}
+             left={{md:"-10.5vw"}}
+             mr={{base:"auto"}}
+             right={{base:"0px"}}
+             height={{base:"2.5vh",md:"5vh"}}
+             width={{base:"3.5vw",md:"7vw"}}
+             mt="15px" 
+             _hover={{ bg: "#2a9ca1" }}
+             fontSize={{base:"3xs",md:"medium"}}
+             ml={{md:"74vw"}} >
+            Next
+            </Button>
+          </Link>
    
-    </div>
+    </Box>
   )
 }
 
