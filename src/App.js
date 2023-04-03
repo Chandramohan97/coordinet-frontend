e8687f5637b70f21c563566616a2935ba926945b
Chandramohan97 <chandramsriv@gmail.com>
2023-04-04 04:06:49 +0530

    feat : importing separate pages on to Homepage.js

--- src/App.js
+++ src/App.js
@@ -6,39 +6,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./Components/LoginForm/LoginForm";
 import TeamStrength from "./pages/TeamStrength/TeamStrength";
 import JobRole from "./pages/JobRole/JobRole";
-import ProjectCreation from "./pages/Sample Table DashBoard/ProjectCreation";
+import ProjectCreation from "./pages/ProjectCreation/ProjectCreation";
 import TaskCreation from "./pages/TaskCreation/TaskCreation";
 import ViewingOptions from "./pages/ViewingOptions/ViewingOptions";
 import EmailVerification from "./pages/EmailVerification/EmailVerification"
+import TableView from "./pages/TableView/TableView";
 
 
 function App() {
   return (
     <div className="App">
       {/* <App1 */}
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signUp" element={<SignUp/>}/>
-        <Route path="/emailVerification" element={<EmailVerification/>} />
-        <Route path="/teamStrength" element={<TeamStrength/>} />
-        <Route path="/jobRole" element={<JobRole/>} />
-      </Routes>
-    </Router>
-    
-    {/* <Router >
-      <Routes>
-        <Route path="/" element={<ProjectCreation/>} />
-        <Route path="/taskCreation" element={<TaskCreation/>} />
-        <Route path="/viewingOptions" element={<ViewingOptions/>} /> */}
-        {/* <Route path="/viewingOption" element={<ViewingOptions/>} /> */}
-      {/* </Routes>
-    </Router> */}
-    {/* <ViewingOptions/> */}
-    {/* <TaskCreation/> */}
-     {/* <App1/>  */}
-       </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/signUp" element={<SignUp/>}/>
+          <Route path="/emailVerification" element={<EmailVerification/>} />
+          <Route path="/teamStrength" element={<TeamStrength/>} />
+          <Route path="/projectCreation" element={<ProjectCreation/>} />
+          <Route path="/taskCreation" element={<TaskCreation/>} />
+          <Route path="/viewingOptions" element={<ViewingOptions/>} /> 
+          <Route path="/tableView" element={<TableView />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
