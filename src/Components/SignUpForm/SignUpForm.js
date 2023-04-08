8511cb34def80b8d2acc5bec95d70c438721a231
Chandramohan97 <chandramsriv@gmail.com>
2023-04-08 23:59:13 +0530

    feat : adding toast to SignUpForm.js

--- src/Components/SignUpForm/SignUpForm.js
+++ src/Components/SignUpForm/SignUpForm.js
@@ -1,6 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React,{useState} from "react";
+// import {  } from "react-router-dom";
+// import {useHistory} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import CustomButton from "../button/CustomButton";
+import axios from "axios";
+
 // import { Button } from "@chakra-ui/react";
 import {
   Box,
@@ -10,95 +14,162 @@ import {
   Flex,
   Button,
   Image,
-  Input
+  Input,
+  useToast
 } from "@chakra-ui/react";
 import googleImage from '../../Asssets/googleImage.png'
 
 const SignUpForm = () => {
 
+  const navigate = useNavigate();
+
+  const toast = useToast();
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [mobNo,setMobNo] = useState('');
+
+  const handleContinue = () =>{
+
+    const nameRegex =/^[A-Za-z ]+$/;   //Regular expressions to check 
+    const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  //Regular expressions to check for emails.
+    const mobRegex = /^\d{10}$/; // Regular expression to check for a 10-digit mobile number.
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d\s]).{8,}$/;
+ // Regular expression to check for password.  
+
+    if (!nameRegex.test(name)) {
+      toast({
+        title: "Invalid name",
+        description: "Name should only contain alphabets.",
+        status: "error",
+        duration: 2000,
+        // isClosable: false,
+        variant:"green"
+      });
+    } else if (!mobRegex.test(mobNo)) {
+      toast({
+        title: "Invalid mobile number",
+        description: "Mobile number should be 10 digits long.",
+        status: "error",
+        duration: 2000,
+        isClosable: true,
+        variant:"green"
+      });
+    } else if (!emailRegex.test(email)) {
+      toast({
+        title: "Invalid email",
+        description: "Please enter a valid email address.",
+        status: "error",
+        duration: 2000,
+        // isClosable: true,
+        variant:"green"
+      });
+    } else if (!passwordRegex.test(password)) {
+      toast({
+        title: "Invalid password",
+        description:
+          "Password should be at least 8 characters long, containing at least one uppercase letter, one lowercase letter, and one digit.",
+        status: "error",
+        duration: 2000,
+        // isClosable: true,
+        variant:"green"
+      });
+    } else {
+      navigate('/emailVerification');
+    }
+  }
+  
   return (
     <Box display="flex" justifyContent="center" alignItems="center">
       <Flex flexDirection="column" spacing="30px">
-        <Heading as="h3" fontFamily="Inder sans-setif" fontSize={{base:"medium",md:"32px"}}>
+        <Heading as="h3" fontFamily="Inder sans-setif" fontSize={{base:"32px",md:"32px"}}>
           Sign Up
         </Heading>
-      <form>
+      <form >
         <Flex spacing="20px" flexDirection="column">
 
         <FormControl id="name" isRequired>
-            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"2xs",md:"medium"}} mx="11px" mt="20px">
+            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"large",md:"medium"}} mx="11px" mt="20px">
               Name
             </FormLabel>
             <Input
               type="text"
               height={{base:"4.2vh",md:"5vh"}}
-              width="20vw"
+              width={{ base: "60vw", md: "20vw" }}
               borderRadius="5px"
-              fontSize={{base:"3xs",md:"medium"}}
+              fontSize={{base:"medium",md:"medium"}}
               placeholder="Enter name"
               textAlign={"left"}
+              onChange={(e)=>setName(e.target.value)}
             ></Input>
           </FormControl>
           <FormControl id="email" isRequired>
-            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"2xs",md:"medium"}} mx="11px" mt="20px">
+            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"large",md:"medium"}} mx="11px" mt="20px">
               Mobile Number
             </FormLabel>
             <Input
-              type="text"
+              type="number"
               height={{base:"4.2vh",md:"5vh"}}
-              width="20vw"
+              fontSize={{base:"medium",md:"medium"}}
               borderRadius="5px"
-              fontSize={{base:"3xs",md:"medium"}}
+              // fontSize={{base:"3xs",md:"medium"}}
               placeholder="Enter mob no"
               textAlign={"left"}
+              onChange={(e)=>setMobNo(e.target.value)}
             ></Input>
           </FormControl>
           <FormControl id="email" isRequired>
-            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"2xs",md:"medium"}} mx="11px" mt="20px">
+            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"large",md:"medium"}} mx="11px" mt="20px">
               Email
             </FormLabel>
             <Input
               type="text"
               height={{base:"4.2vh",md:"5vh"}}
-              width="20vw"
+              fontSize={{base:"medium",md:"medium"}}
               borderRadius="5px"
-              fontSize={{base:"3xs",md:"medium"}}
+              // fontSize={{base:"3xs",md:"medium"}}
               placeholder="Enter email"
               textAlign={"left"}
+              onChange={(e)=>setEmail(e.target.value)}
             ></Input>
           </FormControl>
 
           <FormControl id="password" isRequired>
-            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"2xs",md:"medium"}} mx="11px" mt="10px">
+            <FormLabel fontFamily={"Inder sans-serif"} fontSize={{base:"large",md:"medium"}} mx="11px" mt="10px">
               Password
             </FormLabel>
             <Input
               textAlign={"left"}
               type="password"
               height={{base:"4.2vh",md:"5vh"}}
-              width="20vw"
+              fontSize={{base:"medium",md:"medium"}}
               borderRadius="5px"
-              fontSize={{base:"small",md:"medium"}}
+              // fontSize={{base:"small",md:"medium"}}
               textIndent="0.25rem"
               placeholder="********"
+              onChange={(e)=>setPassword(e.target.value)}
             ></Input>
           </FormControl>
+            {/* <Link to="/emailVerification"> */}
+              <Button
+                color="white"
+                height={{base:"4.2vh",md:"5vh"}}
+                fontSize={{base:"medium",md:"medium"}}
+                bg="#2a9ca1"
+                _hover={{ bg: "#1C7B7F" }} 
+                textAlign="center"
+                // fontSize={{base:"small",md:"medium"}}
+                margin="30px 10px"
+                borderRadius="5px"   
+                onClick={handleContinue} 
+              >
+              Continue
+              </Button>
+            {/* </Link> */}
         </Flex>
-        <Link to="/emailVerification">
-          <Button
-            color="white"
-            height={{base:"4.2vh",md:"5vh"}}
-            width="20vw"
-            bg="#2a9ca1"
-            _hover={{ bg: "#1C7B7F" }} 
-            textAlign="center"
-            fontSize={{base:"small",md:"medium"}}
-            margin="30px 10px"
-            borderRadius="5px"
-          >
-          Continue
-          </Button>
-        </Link>
+        {/* <Link to="/emailVerification"> */}
+          
+        {/* </Link> */}
       </form>
     </Flex>
 </Box>
