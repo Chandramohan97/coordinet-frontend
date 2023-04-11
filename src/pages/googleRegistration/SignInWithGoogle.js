ae908462edc0f78153bc40cbb663a6d2d59af37d
Chandramohan97 <chandramsriv@gmail.com>
2023-04-11 22:22:26 +0530

    Sign in with Google.js - authenticating through react-google-login.

---
+++ src/pages/googleRegistration/SignInWithGoogle.js
@@ -0,0 +1,44 @@
+// import { GoogleLogin } from 'react-google-login';
+// import { GoogleLogin } from 'react-google-login';
+// import { Box, Button, Flex, Text } from '@chakra-ui/react';
+
+// const responseGoogle = (response) => {
+//   console.log(response);
+// }
+
+// const SignInWithGoogle = () => {
+//   return (
+//     <GoogleLogin
+//       clientId="your_client_id"
+//       buttonText="Sign in with Google"
+//       onSuccess={responseGoogle}
+//       onFailure={responseGoogle}
+//       cookiePolicy={'single_host_origin'}
+//       render={renderProps => (
+//         <Button
+//           onClick={renderProps.onClick}
+//           disabled={renderProps.disabled}
+//           bg="#DB4437"
+//           color="white"
+//           borderRadius="4px"
+//           fontWeight="bold"
+//           fontFamily="sans-serif"
+//           _hover={{ bg: '#CF372D' }}
+//           _active={{ bg: '#B82E24' }}
+//           _focus={{ boxShadow: 'none' }}
+//           w="100%"
+//         >
+//           <Flex align="center" justify="center">
+//             <Text fontSize="sm" mr="2">Sign in with Google</Text>
+//             <Box color="white" w={5} h={5} />
+//           </Flex>
+//         </Button>
+//       )}
+//     />
+//   );
+// };
+    
+  
+
+
+// export default SignInWithGoogle;
