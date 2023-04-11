e1e74ca4156865350a3f5d9a2f3cf2a2b7f4d64c
Chandramohan97 <chandramsriv@gmail.com>
2023-04-11 22:23:13 +0530

    feat : applying Media query on Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,101 +1,115 @@
-import { Flex, Text, Box, Image } from '@chakra-ui/react'
-import CustomButton from '../button/CustomButton'
-import { Link } from 'react-router-dom'
-import img from "../../Asssets/landingPage(1).png"
+  import { Flex, Text, Box, Image} from '@chakra-ui/react'
+  // import { useMediaQuery } from 'react-responsive';
+  import { Link as L2 }  from '@chakra-ui/react'
+  import { Link } from 'react-router-dom'
+  import { Button } from '@chakra-ui/react'
+  import img from "../../Asssets/landingPage(1).png"
+  import './Navbar.css'
+  // import { HamburgerIcon } from '@chakra-ui/icons'
+  import React from 'react'
+  import { useMediaQuery } from 'react-responsive'
 
-import React from 'react'
-import { BlockMapBuilder } from 'draft-js'
+  const Navbar = () => {
 
-const Navbar = () => {
-  return (
-  <Flex flexDirection={"column"}>
-    <Flex my={"30px"} flexDirection={"row"} justifyContent="space-between" background="#F7F8FC">
-        <Text fontFamily="Jacques Francois"
-            fontWeight="regular"
-            fontSize="40px"
-            color="#000000"
-            ml="5vw"
-            // position={"relative"}
-            // left="-100px"
-            >
-            COORDINET
-        </Text>
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    return (
+      <>
+        {isMobile == true ? (
+          <div className='dropdown'>
+            <button marginTop={"2vh"} className="dropbtn">Dropdown</button>
+            <div className="dropdown-content">
+              <a href="#">A1</a>
+              <a href="#">A2</a>
+              <a href="#">A3</a>
+            </div>
+          </div>
+        ) :
+        <Flex flexDirection={"column"}>
+        <Flex my={"30px"}  flexDirection={"row"} justifyContent="space-between" background="#F7F8FC">
+          <L2 fontFamily="Inder sans-serif"
+              fontWeight="regular"
+              textDecoration={"none"}
+              _hover={{textDecoration:"none"}}
+              fontSize={{base:"10px",md:"40px"}}
+              color="#000000"
+              ml="5vw"            
+              >COORDINET</L2>
+          <Flex flexDirection={"row"} justifyContent="space-around" mr={"23vw"} >
+          {/* <Link to="/tableDashboard">  */}
+           <Button
+            color="black"
+            height={"5vh"}
+            width={"7vw"}
+            bg="#2B6CB0"
+            _hover={{ 
+            transform:"translate(0px,-5px)",
+            // borderBottom : "2px solid red",
+            borderColor:"blue"}} 
+            _active={{ bg: "#555555" }}
+            textAlign="center"
+            fontSize={{base:"5px",md:"15px"}}
+            margin="15px 10px"
+            borderRadius="5px"
+          > Home</Button> 
+          {/* </Link> */}
 
-        <Flex flexDirection={"row"} justifyContent="space-around" mr={"23vw"} >
-        <CustomButton
+          <Link to="/login">
+            <Button
+              color="black"
+              height={"5vh"}
+              width={"7vw"}
+              bg="#2B6CB0"
+              _hover={{ 
+              transform:"translate(0px,-5px)",
+              borderBottom : "2px solid red",
+              borderColor:"blue"}}
+              _active={{ bg: "#555555" }}
+              textAlign="center"
+              fontSize={{base:"8px",md:"15px"}}
+              margin="15px 10px"
+              borderRadius="5px"
+            > Login</Button>
+          </Link>
+            <Link to="/signUp">
+              <Button
                 color="black"
-                  height="5vh"
-                  width="7vw"
-                //   bg="#31bfc8"
+                height={"5vh"}
+                width={"7vw"}
+                bg="#2B6CB0"
+                _hover={{ 
+                  transform:"translate(0px,-5px)",
+                  borderBottom : "2px solid red",
+                  borderColor:"blue"}}
+                _active={{ bg: "#555555" }}
+                textAlign="center"
+                fontSize={{base:"5px",md:"15px"}}
+                margin="15px 10px"
+                borderRadius="5px"
+                value="Sign up"
+              >Sign up</Button>
+              </Link>
+                <Button
+                  color="black"
+                  height={"5vh"}
+                  width={"7vw"}
+                  bg="#2B6CB0"
                   _hover={{ 
-                    transform:"translate(0px,-5px)",
-                    borderBottom : "2px solid red",
-                    borderColor:"blue"}} 
+                  transform:"translate(0px,-5px)",
+                  borderBottom : "2px solid red",
+                  borderColor:"blue"}}
                   _active={{ bg: "#555555" }}
                   textAlign="center"
-                  fontSize="15px"
+                  fontSize={{base:"5px",md:"15px"}}
                   margin="15px 10px"
                   borderRadius="5px"
-                  value="Home"
-        />  
-        <Link to="/login">
-            <CustomButton
-                  color="black"
-                    height="5vh"
-                    width="7vw"
-                    // bg="#31bfc8"
-                    _hover={{ 
-                      transform:"translate(0px,-5px)",
-                      borderBottom : "2px solid red",
-                      borderColor:"blue"}}
-                    _active={{ bg: "#555555" }}
-                    textAlign="center"
-                    fontSize="15px"
-                    margin="15px 10px"
-                    borderRadius="5px"
-                    value="Login"
-            />  
-        </Link>
-            <Link to="/signUp">
-                <CustomButton
-                    color="black"
-                    height="5vh"
-                    width="7vw"
-                    // bg="#31bfc8"
-                    _hover={{ 
-                      transform:"translate(0px,-5px)",
-                      borderBottom : "2px solid red",
-                      borderColor:"blue"}}
-                    _active={{ bg: "#555555" }}
-                    textAlign="center"
-                    fontSize="15px"
-                    margin="15px 10px"
-                    borderRadius="5px"
-                    value="Sign up"
-                />
-              </Link>
-              <CustomButton
-                    color="black"
-                    height="5vh"
-                    width="7vw"
-                    // bg="#31bfc8"
-                    _hover={{ 
-                      transform:"translate(0px,-5px)",
-                      borderBottom : "2px solid red",
-                      borderColor:"blue"}}
-                    _active={{ bg: "#555555" }}
-                    textAlign="center"
-                    fontSize="15px"
-                    margin="15px 10px"
-                    borderRadius="5px"
-                    value="About"
-                />
-          </Flex> 
-      </Flex>
-      
-    </Flex>
-  );
-  
-}
+                  >About</Button>
+            </Flex> 
+        </Flex>
+        
+      </Flex>  
+        }
+      </>
+    );
+  };
 
-export default Navbar ;
+  export default Navbar ;
