7762745adb74f608dafc8b4b85b05e24532e1bfa
Chandramohan97 <chandramsriv@gmail.com>
2023-04-11 22:20:19 +0530

    feat : modified Board Data.js

--- src/Components/Kanban/Board/boardData.js
+++ src/Components/Kanban/Board/boardData.js
@@ -3,6 +3,30 @@ import { v4 as uuid } from "uuid";
 
 export const boardData = [
     {
+        id:uuid(),
+        title : 'Planned',
+        cards:[
+            { 
+                id : uuid(),
+                label : "Card 7", 
+                commentList :{},
+              //   status : Board.title,
+                Priority : "",
+                dueDate : "",
+                assignee : ""
+            },
+            { 
+                id :uuid(),
+                label : "Card 8",
+                commentList : {},
+              //   status : this.title,
+                Priority : "",
+                dueDate : "",
+                assignee : ""
+            }
+        ]
+    },  
+    {
       id:uuid(),
       title : 'New',
       cards:[
@@ -46,6 +70,30 @@ export const boardData = [
             // status : this.title,
             Priority : "",
             dueDate : "",          
+            assignee : ""
+        }
+    ]
+},
+{
+    id:uuid(),
+    title : 'Review',
+    cards:[
+        { 
+            id : uuid(),
+            label : "Card 5", 
+            commentList :{},
+          //   status : Board.title,
+            Priority : "",
+            dueDate : "",
+            assignee : ""
+        },
+        { 
+            id :uuid(),
+            label : "Card 6",
+            commentList : {},
+          //   status : this.title,
+            Priority : "",
+            dueDate : "",
             assignee : ""
         }
     ]
