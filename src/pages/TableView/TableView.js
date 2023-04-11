d9400a0f427136c6b56073ae8cedd30c7d910e7f
Chandramohan97 <chandramsriv@gmail.com>
2023-04-11 22:26:55 +0530

    feat : Initial Table View

---
+++ src/pages/TableView/TableView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import StaticTable1 from '../../Components/Table/StaticTable1'
+
+const TableView = () => {
+  return (
+    <div>
+        <StaticTable1 />
+    </div>
+  )
+}
+
+export default TableView
